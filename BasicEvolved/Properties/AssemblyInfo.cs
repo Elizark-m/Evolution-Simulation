337fb13f785213ad0f7fa50fa25c6df8153cf93a
Elizark-m <85119923+Elizark-m@users.noreply.github.com>
2022-03-25 17:17:01 +0400

    Update AssemblyInfo.cs

--- BasicEvolved/Properties/AssemblyInfo.cs
+++ BasicEvolved/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("BasicEvolved")]
+[assembly: AssemblyTitle("Evolution_Simulation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BasicEvolved")]
+[assembly: AssemblyProduct("Evolution_Simulation")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
